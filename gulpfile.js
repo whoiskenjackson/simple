a533d60a1707ccf15c0dfb715150c5850f33de50
Kenneth Jackson <KennethJackson@kennethsmacbook.attlocal.net>
2017-03-30 18:43:46 -0700

    Initial Development of Grid Mixins

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,18 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function () {
-    return gulp.src('./dev/css/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed',includePaths:['./dev/css']}).on('error', sass.logError))
-    .pipe(gulp.dest('./app/css'))
+    return gulp.src('./src/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed',includePaths:['./src']}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
     .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
     }))
-    .pipe(gulp.dest('./app/css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./dev/css/**/*.scss', ['css']);
+  gulp.watch('./src/**/*.scss', ['css']);
 });
 
 gulp.task('default', ['css']);
